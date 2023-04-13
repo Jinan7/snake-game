915f5c07072838f08214c9fa53b1e22d9fe25732
unknown <lakanj25@gmail.com>
2023-04-13 22:44:44 +0200

    set up game window

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+#window dimensions and title
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TITLE = "Snake"
+
+screen = Screen()
+screen.setup(WINDOWWIDTH, WINDOWHEIGHT)
+screen.title(TITLE)
+
+
+screen.exitonclick()
+
