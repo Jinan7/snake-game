e63ff9bd6950c8b9e48cddf356fa8450ee271997
unknown <lakanj25@gmail.com>
2023-04-13 23:28:44 +0200

    create snake class

--- snake.py
+++ snake.py
@@ -1,5 +1,11 @@
 from turtle import Turtle
 from cell import *
 class Snake:
-    def __init__(self):
-        pass
+    def __init__(self, no_of_cells):
+        self.no_of_cells = no_of_cells
+        self.snake = []
+
+    def createSnake(self):
+        for pos in range(0, self.no_of_cells):
+           cell = Cell(10*pos,0,0)
+           self.snake.append(cell);
