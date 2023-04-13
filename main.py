28ac372ee12c38817d956a7209f13a18ff623843
unknown <lakanj25@gmail.com>
2023-04-13 22:56:46 +0200

    turn off live animation on screen

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from turtle import Turtle, Screen
+from cell import Cell
 #window dimensions and title
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 TITLE = "Snake"
 
+#create and initialize window
 screen = Screen()
 screen.setup(WINDOWWIDTH, WINDOWHEIGHT)
 screen.title(TITLE)
+screen.tracer(0)
 
 
+snake = Cell(0,0,0,0,0)
 screen.exitonclick()
 
