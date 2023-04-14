f1f1153dc25b917160d8b24c9c6f862af355a340
unknown <lakanj25@gmail.com>
2023-04-14 11:08:35 +0200

    add game loop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from cell import Cell
+from snake import Snake
+import time
 #window dimensions and title
 WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
@@ -10,10 +12,14 @@ screen = Screen()
 screen.setup(WINDOWWIDTH, WINDOWHEIGHT)
 screen.title(TITLE)
 screen.tracer(0)
+snake = Snake(5)
+
+#game lopp
+while True:
+    snake.move(20,0)
+    screen.update()
+    time.sleep(0.2)
 
 
-snake = Cell(0,0,0)
-
-screen.update()
 screen.exitonclick()
 
