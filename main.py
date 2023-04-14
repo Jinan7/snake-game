6ef117a75ed6d5b2e19300f206b4df27bc5c8cdf
unknown <lakanj25@gmail.com>
2023-04-14 23:12:16 +0200

    add functions to listen for key presses and change snake direction

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ WINDOWWIDTH = 600
 WINDOWHEIGHT = 600
 TITLE = "Snake"
 
+
+
 #create and initialize window
 screen = Screen()
 screen.setup(WINDOWWIDTH, WINDOWHEIGHT)
@@ -14,9 +16,42 @@ screen.title(TITLE)
 screen.tracer(0)
 snake = Snake(5)
 
+#movement variables
+global x_velocity, y_velocity
+x_velocity = 10
+y_velocity = 0
+
+def up():
+    global y_velocity, x_velocity
+    y_velocity = 10
+    x_velocity = 0
+def down():
+    global y_velocity, x_velocity
+    y_velocity = -10
+    x_velocity = 0
+def right():
+    global y_velocity, x_velocity
+    y_velocity = 0
+    x_velocity = 10
+
+def left():
+    global y_velocity, x_velocity
+    y_velocity = 0
+    x_velocity = -10
+
+#listen for key presses
+screen.listen()
+screen.onkey(up, "w")
+screen.onkey(down, "s")
+screen.onkey(right, "d")
+screen.onkey(left, "a")
+
+
+
+
 #game lopp
 while True:
-    snake.move(20,0)
+    snake.move(x_velocity, y_velocity)
     screen.update()
     time.sleep(0.2)
 
