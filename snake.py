2fd08bdb0bf669b367e665856335de828734cbbf
unknown <lakanj25@gmail.com>
2023-04-14 10:08:40 +0200

    create move function

--- snake.py
+++ snake.py
@@ -4,8 +4,14 @@ class Snake:
     def __init__(self, no_of_cells):
         self.no_of_cells = no_of_cells
         self.snake = []
+        self.createSnake()
 
     def createSnake(self):
         for pos in range(0, self.no_of_cells):
-           cell = Cell(10*pos,0,0)
-           self.snake.append(cell);
+            cell = Cell(-20 * pos, 0, 0)
+            self.snake.append(cell)
+    def move(self, x, y):
+        for pos in range(self.no_of_cells-1, 0, -1):
+            self.snake[pos].x = self.snake[pos - 1].x
+            self.snake[pos].y = self.snake[pos - 1].y
+        self.snake[0].set_position(x, y)
